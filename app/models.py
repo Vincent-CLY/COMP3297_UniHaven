fa08e53214496ddcade6329f2c85b2c46e465e19
Lei Kam Fai <97010099+CashonPierre@users.noreply.github.com>
2025-03-31 00:10:21 +0800

    added user, reservation, cancel class

--- app/models.py
+++ app/models.py
@@ -20,3 +20,30 @@ class Accommodation(models.Model):
     latitude = models.FloatField()
     longtitude = models.FloatField()
     geoAdrress = models.CharField(max_length=255)
+
+class User(models.Model):
+    user_id = models.IntegerField(primary_key=True) # Primary key
+    username = models.CharField(max_length=100)
+    password = models.CharField(max_length=100)
+    email = models.CharField(max_length=100)
+
+class HKUStudent(User):
+    HKU_ID = models.CharField(max_length=20, unique=True)
+
+class CEDARSStaff(User):
+    staff_id = models.CharField(max_length=20, unique=True)
+    staff_department = models.CharField(max_length=100)
+
+class Reservation(models.Model):
+    reservation_id = models.IntegerField(primary_key=True) # Primary key
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE) # Foreign key
+    accommodation_id = models.ForeignKey(Accommodation, on_delete=models.CASCADE) # Foreign key
+    reservation_date = models.DateTimeField()
+    check_in_date = models.DateTimeField()
+    check_out_date = models.DateTimeField()
+    is_cancelled = models.BooleanField(default=False)
+
+class CancelledReservation(models.Model):
+    cancellation_id = models.IntegerField(primary_key=True) # Primary key
+    reservation_id = models.ForeignKey(Reservation, on_delete=models.CASCADE) # Foreign key
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE) # Foreign key [canncelled by user]
