ba4169f7f50cc3d62667b8858d2fbeff0f188078
Chung Lai Yin Vincent <122752877+Vincent-CLY@users.noreply.github.com>
2025-03-30 14:19:54 +0000

    Rename from accommodation to app

--- accommodation/migrations/0002_rename_ownerdetails_accommodation_owner_details.py
+++ accommodation/migrations/0002_rename_ownerdetails_accommodation_owner_details.py
@@ -6,7 +6,7 @@ from django.db import migrations
 class Migration(migrations.Migration):
 
     dependencies = [
-        ("accommodation", "0001_initial"),
+        ("app", "0001_initial"),
     ]
 
     operations = [
--- unihaven/settings.py
+++ unihaven/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    "accommodation.apps.AccommodationConfig",
+    "app.apps.AccommodationConfig",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
