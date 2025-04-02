a51315bbbb0a6cbb7ca44e96d47efb71b77a83e6
Chung Lai Yin Vincent <122752877+Vincent-CLY@users.noreply.github.com>
2025-04-02 10:42:37 +0000

    Reconstrcuted the cancel_reservation, added new functionality of creating notification and partially update reservation's attribute (is_cancelled)

--- app/api_views.py
+++ app/api_views.py
@@ -1,9 +1,9 @@
-from rest_framework import generics
-from rest_framework import filters
+from rest_framework import generics, filters, status
+from rest_framework.response import Response
 from django_filters.rest_framework import DjangoFilterBackend
 from .filters import AccommodationFilter
 from .models import Accommodation, Reservation, CancelledReservation
-from .serializers import SimpleAccommodationSerializer, DetailedAccommodationSerializer, ReservationSerializer, CancelledReservationSerializer
+from .serializers import SimpleAccommodationSerializer, DetailedAccommodationSerializer, ReservationSerializer, CancelledReservationSerializer, NotificationSerializer
 
 class list_accommodations(generics.ListAPIView):
     queryset = Accommodation.objects.filter(is_available="True")
@@ -21,7 +21,55 @@ class create_reservation(generics.CreateAPIView):
     model = Reservation
     serializer_class = ReservationSerializer
 
-class cancel_reservation(generics.CreateAPIView):
-    model = CancelledReservation
+class cancel_reservation(generics.GenericAPIView):
+    queryset = CancelledReservation.objects.all()
     serializer_class = CancelledReservationSerializer
+
+    def post(self, request, pk, *args, **kwargs):
+        try:
+            reservation = Reservation.objects.get(pk=pk)
+            print(f"Reservation: {reservation.reservation_id}")
+            accommodation = Accommodation.objects.get(accommodation_id=reservation.accommodation_id.accommodation_id)
+            # print(f"Accommodation ID: {Accommodation.objects.get(accommodation_id=reservation.accommodation_id).accommodation_id}")
+            # print(f"Accommodation name: {Accommodation.objects.get(accommodation_id=reservation.accommodation_id).name}")
+            cancelled_data = {
+                "reservation_id" : reservation.reservation_id,
+                "user_id" : reservation.user_id.user_id
+            }
+            cancelled_serializer = CancelledReservationSerializer(data=cancelled_data)
+            cancelled_serializer.is_valid(raise_exception=True)
+            cancelled_record = cancelled_serializer.save()
+
+            notification_data = {
+                "user_id" : reservation.user_id.user_id,
+                "type" : "Cancel",
+                "notification_content" : f"Your reservation of {accommodation.name} has been cancelled."
+            }
+            notification_serializer = NotificationSerializer(data=notification_data)
+            notification_serializer.is_valid(raise_exception=True)
+            notification_record = notification_serializer.save()
+
+            reservation.is_cancelled = True
+            reservation.save()
+
+            return Response(
+                {
+                    "cancelled_reservation" : CancelledReservationSerializer(cancelled_record).data,
+                    "notification" : NotificationSerializer(notification_record).data,
+                    "reservation_update" : {
+                        "id" : reservation.reservation_id,
+                        "is_cancelled" : reservation.is_cancelled
+                    }
+                },
+                status = status.HTTP_201_CREATED,
+            )
+        except Reservation.DoesNotExist:
+            return Response(
+                {"error" : "Reservation not found."},
+                status = status.HTTP_404_NOT_FOUND,
+            )
+
+
+
+    
         
