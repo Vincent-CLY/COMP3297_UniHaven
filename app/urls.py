97a3bd1c458fbcdf0c120944f22d22666969c10b
Chung Lai Yin Vincent <122752877+Vincent-CLY@users.noreply.github.com>
2025-04-11 04:39:12 +0000

    Add redirect to default list accommodations, and remove <int: pk> for cancel reservation

--- app/urls.py
+++ app/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
+from django.views.generic import RedirectView
 from .api_views import list_accommodations, detailed_accommodation, create_reservation, cancel_reservation
 
 urlpatterns = [
+    path("", RedirectView.as_view(url="api/accommodations", permanent=False), name="home"),
     path("api/accommodations", list_accommodations.as_view(), name="list_accommodations"),
     path("api/accommodations/<int:pk>", detailed_accommodation.as_view(), name="accommodation_detail"),
     path("api/accommodations/reserve/<int:pk>", create_reservation.as_view(), name="accommodation_detail"),
-    path("api/accommodations/cancel/<int:pk>", cancel_reservation.as_view(), name="accommodation_detail"),
+    path("api/accommodations/cancel/", cancel_reservation.as_view(), name="accommodation_detail"),
 
 ]
